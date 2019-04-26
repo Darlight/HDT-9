25e0fe4b2d3778896d7066749b4fd4d8f6e63cec
Darlight <andres.perdomo09@gmail.com>
2019-04-26 11:55:08 -0600

    Push

--- Diccionario/src/FactoryTree.java
+++ Diccionario/src/FactoryTree.java
@@ -1,2 +1,16 @@
-public class FactoryTree {
+public class FactoryTree<E> {
+    //Factory that creates instances
+    public E getTree(String treeType) {
+        if (treeType == null) {
+            return null;
+        }
+        if (treeType.equalsIgnoreCase("SPLAY")) {
+            return new SplayTree();
+
+        } else if (treeType.equalsIgnoreCase("RBT")) {
+            return new RBTree();
+        }
+        return null;
+    }
 }
+
